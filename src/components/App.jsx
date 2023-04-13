429009c65d766452163126d4455ca6666f14ceac
Qube <k.twerdochlib@gmail.com>
2023-04-13 20:33:12 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Home />
+      <Movies />
+      <MovieDetails />
+      <Cast />
+      <Reviews />
     </div>
   );
 };
